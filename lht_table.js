d6e9766e0cea65d5b52572bcae9664a9a2f43262
DM232 <143013580+DM232@users.noreply.github.com>
2024-02-13 10:38:55 -0600

    Add files via upload

---
+++ lht_table.js
@@ -0,0 +1,19 @@
+"use strict";
+
+/*
+   New Perspectives on HTML5 and CSS3, 7th Edition
+   Tutorial 10
+   Review Assignment
+
+   Author: Drake McIntyre
+   Date: 1/23/2024
+
+	
+*/
+
+var thisDay = new Date("August 30, 2018");
+
+<table id='eventTable'>
+   <caption>Upcoming Events</caption>
+   <tr><th>Date</th><th>Event</th><th>Prive</th></tr>
+
