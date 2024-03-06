d6868b4e37adf963835ba210bc8be57ecacea326
DM232 <143013580+DM232@users.noreply.github.com>
2024-03-06 10:13:15 -0600

    Add files via upload

--- lht_table.js
+++ lht_table.js
@@ -13,7 +13,28 @@
 
 var thisDay = new Date("August 30, 2018");
 
-<table id='eventTable'>
-   <caption>Upcoming Events</caption>
-   <tr><th>Date</th><th>Event</th><th>Prive</th></tr>
+var tableHTML = "<table id='eventTable'>"
+tableHTML +="<caption>Upcoming Events</caption>";
+tableHTML +="<tr><th>Date</th><th>Event</th><th>Prive</th></tr>";
 
+var endDate = new Date(thisDay.getTime() + 14*24*60*60*1000);
+
+for(var i = 0; i < eventDates.length; i++) {
+   var eventDate = new Date(eventDates[i]);
+   var eventDay = eventDate.toDateString();
+   var eventTime = eventDate.toLocaleTimeString();
+
+   if ((thisDay <= eventDate) && (eventDate <= endDate)) {
+      tableHTML += "<tr>";
+      tableHTML += "<td>" + eventDay + " 0 " + eventTime + "</td>";
+      tableHTML += "<td>" + eventDescriptions[i] + "</td>";
+      tableHTML += "<td>" + eventPrices[i] + "</td>";
+      tableHTML += "</tr>";
+   }
+
+}
+
+tableHTML += "</table>";
+
+
+document.getElementById("eventList").innerHTML = tableHTML;
